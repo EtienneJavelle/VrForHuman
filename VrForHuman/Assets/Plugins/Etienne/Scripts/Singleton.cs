df9bbea8cbca04e200d9212b63723c10a38d3854
EtienneJavelle <75083138+EtienneJavelle@users.noreply.github.com>
2021-08-11 16:09:44 +0200

    Update Singleton.cs
    Erreur quand on met un singleton en enfant de quelque chose

--- VrForHuman/Assets/Plugins/Etienne/Scripts/Singleton.cs
+++ VrForHuman/Assets/Plugins/Etienne/Scripts/Singleton.cs
@@ -9,13 +9,17 @@ namespace Etienne {
 
         protected virtual void Awake() {
             if(Singleton<T>.instance != null) {
-                Debug.LogError($"There are more than one \"{typeof(T).Name}\" singleton.", gameObject);
+                Debug.LogError($"There are more than one <b>\"{typeof(T).Name}\"</b> singleton.", gameObject);
                 GameObject.Destroy(this);
                 return;
             }
             Singleton<T>.instance = this as T;
 
             if(isPersistant) {
+                if(transform.parent != null) {
+                    transform.parent = null;
+                    Debug.LogWarning($"Singleton <b>\"{typeof(T).Name}\"</b> is a child, it has been removed from his parent.", gameObject);
+                }
                 DontDestroyOnLoad(this);
             }
         }
