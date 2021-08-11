c13e9c58fae7f040227553c79915aef15e2ffbaf
EtienneJavelle <75083138+EtienneJavelle@users.noreply.github.com>
2021-08-11 16:03:19 +0200

    Update DefaultSceneLoader.cs

--- VrForHuman/Assets/Editor/DefaultSceneLoader.cs
+++ VrForHuman/Assets/Editor/DefaultSceneLoader.cs
@@ -1,19 +1,34 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [InitializeOnLoad, ExecuteAlways]
 public static class DefaultSceneLoader {
+    private const string keySceneToLoadBack = "SceneToLoadBack";
+
     static DefaultSceneLoader() {
         EditorApplication.playModeStateChanged += LoadDefaultScene;
     }
 
     private static void LoadDefaultScene(PlayModeStateChange state) {
-        if(state == PlayModeStateChange.EnteredPlayMode) {
-            if(EditorUtility.DisplayDialog("Default Scene Loader Is Active", "Load first build scene?", "Yes", "No")) {
-                Etienne.Utils.ClearLog();
-                EditorSceneManager.UnloadSceneAsync(EditorSceneManager.GetActiveScene());
-                EditorSceneManager.LoadScene(0);
+        if(state == PlayModeStateChange.ExitingEditMode) {
+            Scene currentScene = EditorSceneManager.GetActiveScene();
+            string defaultScenePath = EditorBuildSettings.scenes[0].path;
+            if(currentScene.path != defaultScenePath) {
+                if(EditorUtility.DisplayDialog("Default Scene Loader Is Active", "Load first build scene?", "Yes", "No")) {
+                    PlayerPrefs.SetString(keySceneToLoadBack, currentScene.path);
+                    EditorSceneManager.OpenScene(defaultScenePath, OpenSceneMode.Single);
+                }
+            } else {
+                PlayerPrefs.DeleteKey(keySceneToLoadBack);
+            }
+        }
+        if(state == PlayModeStateChange.EnteredEditMode) {
+            if(PlayerPrefs.HasKey(keySceneToLoadBack)) {
+                string sceneToLoadBack = PlayerPrefs.GetString(keySceneToLoadBack);
+                EditorSceneManager.OpenScene(sceneToLoadBack, OpenSceneMode.Single);
+                PlayerPrefs.DeleteKey(keySceneToLoadBack);
             }
         }
     }
