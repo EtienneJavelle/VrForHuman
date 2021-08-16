17fa5d519e682d2f682aecd6d8d18ea54fd5f7a0
EtienneJavelle <75083138+EtienneJavelle@users.noreply.github.com>
2021-08-16 10:20:12 +0200

    Update PlayerCanvasManager.cs

--- VrForHuman/Assets/Scripts/UI/PlayerCanvasManager.cs
+++ VrForHuman/Assets/Scripts/UI/PlayerCanvasManager.cs
@@ -1,12 +1,19 @@
+using DG.Tweening;
+using Etienne;
 using UnityEngine;
 
-public class PlayerCanvasManager : MonoBehaviour {
+[Requirement(typeof(AudioManager))]
+public class PlayerCanvasManager : MonoBehaviourWithRequirement {
     #region UnityInspector
 
     [SerializeField] private TextFadingDisplay cityDisplay;
     [SerializeField] private GameObject endSimulationDisplay;
+    [SerializeField] private UnityEngine.UI.Image blood;
+    [SerializeField] private Sound hurtSound;
 
     #endregion
+
+    private bool canBeHurt = true;
 
     private void Awake() {
         GameManager.Instance.SetPlayerCanvasManager(this);
@@ -18,5 +25,16 @@ public class PlayerCanvasManager : MonoBehaviour {
 
     public void ActiveCityDisplay(bool _value) {
         cityDisplay.gameObject.SetActive(_value);
+    }
+
+    public void BeHurt() {
+        if(!canBeHurt) return;
+        canBeHurt = false;
+        AudioManager.Play(hurtSound);
+        blood.DOComplete();
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(blood.DOFade(.8f, .1f).SetEase(Ease.OutElastic));
+        sequence.Append(blood.DOFade(.1f, .9f).OnComplete(() => canBeHurt = true));
+        sequence.Play();
     }
 }
